bb24ce2f49ecb4308662af561b9ffe569c37dadf
DESKTOP-9MT21FL\xiaoYown <qzy09101018@sina.com>
2017-06-12 23:24:18 +0800

    Mod_indent 修改

--- src/tools/utils.js
+++ src/tools/utils.js
@@ -1,51 +1,51 @@
-//Ajax的简单封装
-export function httpResponse(api,success,error) {
-  var _this=this;
+// Ajax的简单封装
+export function httpResponse (api, success, error) {
   api.then(response => {
-    success && success(response);
-  },response => {
+    success && success(response)
+  }, response => {
     error && error(response.status)
-    if(response.status==404 || response.status==500){
-        console.log('服务器无响应-'+response.status)
+    if (response.status === 404 || response.status === 500) {
+      console.log('服务器无响应-' + response.status)
     }
   })
 }
 
 // arguments.length == 1 提取属性,返回新对象
 // arguments.length == 2 提取属性,覆盖原对象属性,返回属性覆盖后对象
-export function getAttribute(obj_1, obj_2 ){
-  obj_2 = obj_2 || {};
-  for( let key in obj_1 ){   // 只提取需要覆盖的属性
-    if( !!obj_1[key] && typeof obj_1[key] == 'object' ){
-      if( Array.isArray(obj_1[key]) )
-        obj_2[key] = obj_1[key];
-      else
-        obj_2[key] = getAttribute(obj_1[key]);
+export function getAttribute (obj1, obj2) {
+  obj2 = obj2 || {}
+  // 只提取需要覆盖的属性
+  for (let key in obj1) {
+    if (!!obj1[key] && typeof obj1[key] === 'object') {
+      if (Array.isArray(obj1[key])) {
+        obj2[key] = obj1[key]
+      } else {
+        obj2[key] = getAttribute(obj1[key])
+      }
     } else {
-      obj_2[key] = obj_1[key];
+      obj2[key] = obj1[key]
     }
   }
-  return obj_2
+  return obj2
 }
 // 只覆盖第一个参数本身含有的属性
-export function saveAttribute(obj_1, obj_2){
-  for( let key in obj_1 ){
-
-    if( obj_1[key].constructor != Object ){
-      if( obj_2[key] != undefined ){
-        obj_1[key] = obj_2[key];
+export function saveAttribute (obj1, obj2) {
+  for (let key in obj1) {
+    if (obj1[key].constructor !== Object) {
+      if (obj2[key] !== undefined) {
+        obj1[key] = obj2[key]
       }
-    } else if( obj_2[key] != undefined ) {
-      obj_1[key] = saveAttribute(obj_1[key], obj_2[key]);
+    } else if (obj2[key] !== undefined) {
+      obj1[key] = saveAttribute(obj1[key], obj2[key])
     }
   }
-  return obj_1
+  return obj1
 }
 // 检查是狗为空对象
-export function isEmpty(obj){
-  if( typeof obj != 'object' ){
-    console.error('It is not a Object');
+export function isEmpty (obj) {
+  if (typeof obj !== 'object') {
+    console.error('It is not a Object')
     return true
   }
-  return Object.keys(obj).length == 0
+  return Object.keys(obj).length === 0
 }
